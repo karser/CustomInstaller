a50950e6ecbe65bf987dbc48428f03e7bcd5e21b
karser <dpoddubny@gmail.com>
2013-02-06 13:33:40 +0400

    Symfony2Installer

--- Symfony2Installer.php
+++ Symfony2Installer.php
@@ -1,25 +1,43 @@
 <?php
 namespace Karser\CustomInstaller;
 
-use Composer\Installers\BaseInstaller;
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
 
-class Symfony2Installer extends BaseInstaller
+class Symfony2Installer extends LibraryInstaller
 {
-    protected $locations = array(
-        'bundle' => 'src/{$vendor}/{$name}/',
-    );
-
     protected function mapCustomInstallPaths(array $paths, $name)
     {
         // allow for wildcard in name, e.g., "vendor/*-bundle"
         foreach ($paths as $path => $names) {
             $pattern = str_replace('*', '.*', '{^(' . implode('|', $names) . ')$}');
-
             if (preg_match($pattern, $name)) {
                 return $path;
             }
         }
 
         return false;
+    }
+
+    public function getInstallPath(PackageInterface $package)
+    {
+        if (!empty($extra['installer-paths'])) {
+            $prettyName = $package->getPrettyName();
+            $customPath = $this->mapCustomInstallPaths($extra['installer-paths'], $prettyName);
+            if ($customPath !== false) {
+                list($vendor, $name) = explode('/', $prettyName);
+                $vendor = ucfirst($vendor);
+                $name = ucfirst($name);
+
+                return 'src/' . $vendor . '/' . $name . '/';
+            }
+        }
+
+        return parent::getInstallPath($package);
+    }
+
+    public function supports($packageType)
+    {
+        return true;
     }
 }
